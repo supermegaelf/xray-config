7df01cf9967bdbdeb4883bd3f1e30e4e16cb5437
supermegaelf <81654098+supermegaelf@users.noreply.github.com>
2025-01-31 02:33:19 +0300

    Add files via upload

---
+++ xray-config.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+cat > "/var/lib/marzban/xray_config.json" <<EOF
+{
+  "log": {
+    "access": "/var/lib/marzban/access.log",
+    "error": "/var/lib/marzban/error.log",
+    "loglevel": "warning",
+    "dnsLog": true
+  },
+  "routing": {
+    "domainStrategy": "IPIfNonMatch",
+    "rules": [
+      {
+        "ip": ["geoip:private"],
+        "outboundTag": "BLOCK",
+        "type": "field"
+      },
+      {
+        "type": "field",
+        "outboundTag": "IPv4",
+        "domain": ["geosite:google"]
+      },
+      {
+        "protocol": ["bittorrent"],
+        "outboundTag": "BLOCK",
+        "type": "field"
+      }
+    ]
+  },
+  "inbounds": [
+    {
+      "tag": "Shadowsocks TCP",
+      "listen": "0.0.0.0",
+      "port": 1080,
+      "protocol": "shadowsocks",
+      "settings": {
+        "clients": [],
+        "network": "tcp,udp"
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom",
+      "tag": "DIRECT"
+    },
+    {
+      "protocol": "blackhole",
+      "tag": "BLOCK"
+    },
+    {
+      "tag": "IPv4",
+      "protocol": "freedom",
+      "settings": {
+        "domainStrategy": "ForceIPv4"
+      }
+    }
+  ]
+}
+EOF
+
+marzban restart
